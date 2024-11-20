209f9036aeb96f0bdd3c9af007f24e36283ed098
IkerBe48 <ikerbm@msmk.university>
2024-11-20 21:01:12 +0100

    Creacion de las clases Paciente y Medico

--- Hospital.cpp
+++ Hospital.cpp
@@ -2,11 +2,36 @@
 //
 
 #include "Hospital.h"
+#include <iostream>
 
 using namespace std;
+
+// Clase Paciente
+class Paciente {
+public:
+
+    int id;
+    string nombre;
+    string fechaIngreso;
+    string historialClinico;
+
+};
+
+// Clase Medico
+class Medico {
+public:
+
+    int id;
+    string nombre;
+    string especialidad;
+    bool disponible;
+
+};
 
 int main()
 {
 	cout << "Hello CMake." << endl;
 	return 0;
 }
+
+
