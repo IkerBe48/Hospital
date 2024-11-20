5ed51384e15599a9b917201b0ca0510173d3a19a
IkerBe48 <ikerbm@msmk.university>
2024-11-20 20:49:50 +0100

    Agregar archivos de proyecto.

---
+++ Hospital.cpp
@@ -0,0 +1,12 @@
+﻿// Hospital.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Hospital.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
